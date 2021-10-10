3183143b3348dd4ca065b558a50db550055634cb
Serge Zaugg <serge.zaugg@gmail.com>
2021-10-10 17:53:25 +0200

    work in progress

--- README.md
+++ README.md
@@ -8,13 +8,25 @@ ffmpg
 ## Python dependencies
 
 
+# misc 
+python -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --upgrade build
+
+python -m build
 
 
 
 
+## --------------
+#CC BY
+#CC BY-SA 
+#CC BY-ND # cannot be edited 
+#CC BY-NC
+#CC BY-NC-SA 
+#CC BY-NC-ND # cannot be edited 
 
-
-
+#search_str = 'nr:388556'
+# search_str = 'Attila+bolivianus'
 
 
 
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='xcproc',  
+    version='0.0.8',
+    scripts=['src/xcp_download'],
+    author="Serge Zaugg",
+    author_email="serge.zaugg@gmail.com",
+    description="Python app to download and organize files from Xeno-Canto",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://xxxxxxx",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'requests', 'pandas', 'unidecode',
+      ],
+ )
+
+
