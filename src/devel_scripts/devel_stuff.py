cde7bf5680723dd16ab92d4d0a516a84c5ac9666
Serge Zaugg <serge.zaugg@gmail.com>
2021-10-13 11:17:05 +0200

    work in progress

--- src/devel_scripts/devel_stuff.py
+++ src/devel_scripts/devel_stuff.py
@@ -1,7 +1,11 @@
 
 
-# 
-# ./src/xco_download  /home/serge/sz_main/ml/data/xc_spec_03/import_params01.json
+# cd to where stuff will be downloaded 
+# cd /home/serge/sz_main/ml/data/xc_spec_03
 
+# run script devel 
+# /home/serge/sz_main/ml/src/xeno_canto_organizer/src/xco_download     import_params01.json  -b 
 
+# run script if installed
+# xco_download  import_params01.json
 
