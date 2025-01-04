565307f1e03e079e6e7fc7a09f42b76d25fea4be
Serge Zaugg <serge.zaugg@gmail.com>
2025-01-04 17:21:53 +0100

    mm

--- 02_feature_extract.py
+++ 02_feature_extract.py
@@ -6,19 +6,21 @@ import numpy as np
 import gc
 import re 
 import pandas as pd
-# from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 import shutil
 import os
 from PIL import Image
 
-
+from skimage.measure import block_reduce
 
 # C:\Users\sezau\Desktop\proj2\downloaded_data_img_24000sps
 
 ################################
 # FEATURE EXTRACTION
-pa = os.path.join('C:\\Users\\sezau\\Desktop\\proj2', 'downloaded_data_img_24000sps')
+# pa = os.path.join('C:\\Users\\sezau\\Desktop\\proj2', 'downloaded_data_img_24000sps')
+pa = os.path.join('C:\\Users\\sezau\\Desktop\\project01', 'downloaded_data_img_24000sps')
+
 
 # first load all images 
 imgs = []
@@ -37,39 +39,20 @@ len(imgs)
 im_stack = np.array([a[1] for a in imgs])
 im_stack.shape
 
-
-
-
-# predict by batches 
-batch_size = 32
-feature_vect = []
-finle_name_li = []
-for i in np.arange(0,len(imgs), batch_size):
-    print(i)
-    # predict a batch of images 
-    img_li = imgs[(i):(i+batch_size)]
-    # extract file name
-    finam_li = [a[0] for a in img_li]
-    finle_name_li.extend(finam_li)
-    # extract features 
-    img_batch = [a[1] for a in img_li]
-    imgs_arr = np.array(img_batch)
-    imgs_arr.shape
-    imgs_arr = torch.from_numpy(imgs_arr)
-    batch = preprocess(imgs_arr)
-    feature_vect_i = model(batch)
-    del(batch, imgs_arr, img_batch)
-    gc.collect()
-    print(feature_vect_i.shape)
-    # convert to numpy as copy 
-    feat_arr = feature_vect_i.cpu().detach().numpy() 
-    feature_vect.append(feat_arr)
-    del(feat_arr)
-    gc.collect()
-
 # post process 
-feature_mat = np.concatenate(feature_vect)
-finle_name_arr = np.array(finle_name_li)
+feature_mat_f   = im_stack.mean(axis=2)
+feature_mat_t   = im_stack.mean(axis=1)
+
+# reduce granularits of time features 
+feature_mat_t.shape
+feature_mat_t = block_reduce(feature_mat_t,(1,4), np.mean)
+feature_mat_t.shape
+
+feature_mat_f.shape
+feature_mat_t.shape
+feature_mat = np.concatenate([feature_mat_f, feature_mat_t], axis=1)
+
+finle_name_arr = np.array([a[0] for a in imgs])
 
 
 # feature_mat.shape
@@ -81,11 +64,12 @@ finle_name_arr = np.array(finle_name_li)
 
 
 
+
 ################################
 # AGGLOM CLUSTERING
 
 # clustering 
-clu = AgglomerativeClustering(n_clusters=35, metric='euclidean', linkage='ward')
+clu = AgglomerativeClustering(n_clusters=21, metric='euclidean', linkage='ward')
 
 # clustering = clu.fit(feature_mat)
 cluster_ids = clu.fit_predict(feature_mat)
