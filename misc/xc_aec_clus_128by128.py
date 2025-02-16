eff341c4cedeb4a053f818ca9ee299ea73233548
Serge Zaugg <serge.zaugg@gmail.com>
2025-02-16 20:56:30 +0100

    in misc added small eperiment for aec to clust

--- misc/xc_aec_clus_128by128.py
+++ misc/xc_aec_clus_128by128.py
@@ -6,7 +6,7 @@
 import xco 
 
 # define the root path 
-xc = xco.XCO(start_path = 'C:/xxxxxxxxxxx/xxxxxxxxxxxxxxt')
+xc = xco.XCO(start_path = 'C:/xc_real_projects/xc_aec_project')
 # check where data will be retrieved
 xc.XC_API_URL
 # check where data will be written 
@@ -14,9 +14,9 @@ xc.start_path
 # create a template json parameter file (to be edited)
 xc.make_param(filename = 'aaaaa.json')
 # get information of what would be downloaded
-xc.get(params_json = 'aaaaa.json', download = False)
+xc.get(params_json = 'xc_aec_proj.json', download = False)
 # download mp3 files with metadata  
-xc.get(params_json = 'aaaaa.json', download = True)
+xc.get(params_json = 'xc_aec_proj.json', download = True)
 # convert mp3s to wav with a specific sampling rate (requires ffmpeg to be installed)
 xc.mp3_to_wav(target_fs = 24000)
 # extract spectrograms (get 128*128 arrays)
