46c0ac61c8edfbdadc27f861b35e8dde18a5ec57
Serge Zaugg <serge.zaugg@gmail.com>
2025-04-03 18:10:07 +0200

    only changes in spec

--- spec/spec_parus_01.py
+++ spec/spec_parus_01.py
@@ -5,13 +5,13 @@
 # Make an instance of the XCO class and define the start path 
 import xco 
 
-xc = xco.XCO(start_path = 'C:/xc_real_projects/xc_parus_01')
+xc = xco.XCO(start_path = 'C:/xc_real_projects/xc_parus_02')
 
 # Create a template json parameter file (to be edited)
-# xc.make_param(filename = 'download_sw_europe_small.json', template = "sw_europe_small")
+# xc.make_param(filename = 'download_n_europe_small.json', template = "n_europe")
 
 # Get information of what will be downloaded
-df_records = xc.get_summary(params_json = 'download_parus_01.json')
+df_records = xc.get_summary(params_json = 'download_parus.json')
 
 # make summary tables 
 print(df_records.shape)
@@ -25,14 +25,14 @@ xc.download(df_recs=df_records)
 # Convert mp3s to wav with a specific sampling rate (requires ffmpeg to be installed)
 xc.mp3_to_wav(target_fs = 24000)
 
-# Make spectrogram with size = 128 x 128 
-xc.extract_spectrograms(target_fs = 24000, segm_duration = 0.202, segm_step = 0.5, win_siz = 256, win_olap = 220.5, equalize = True, colormap='gray')
+# # Make spectrogram with size = 128 x 128 
+# xc.extract_spectrograms(target_fs = 24000, segm_duration = 0.202, segm_step = 0.5, win_siz = 256, win_olap = 220.5, equalize = True, colormap='gray')
 
-# Make long spectrogram 128 freq x 1152 time (1024+128= 1152)
-xc.extract_spectrograms(target_fs = 24000, segm_duration = 1.738, segm_step = 0.95, win_siz = 256, win_olap = 220.00, equalize = True, colormap='gray')
+# # Make long spectrogram 128 freq x 1152 time (1024+128= 1152)
+# xc.extract_spectrograms(target_fs = 24000, segm_duration = 1.738, segm_step = 0.95, win_siz = 256, win_olap = 220.00, equalize = True, colormap='gray')
 
 # Make rectangular spectrogram with size = 128 freq x 256 time 
-xc.extract_spectrograms(target_fs = 24000, segm_duration = 0.394 , segm_step = 0.25, win_siz = 256, win_olap = 220.5, equalize = True, colormap='gray')
+xc.extract_spectrograms(target_fs = 24000, segm_duration = 0.394 , segm_step = 0.80, win_siz = 256, win_olap = 220.5, equalize = True, colormap='gray')
 
 
 
