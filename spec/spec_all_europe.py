6732e7877063e7ff7d22951cec56809456bcd31d
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-08 22:12:36 +0200

    misc

---
+++ spec/spec_all_europe.py
@@ -0,0 +1,30 @@
+#----------------------
+# Author : Serge Zaugg
+# Description : Example of xco usage in practice 
+#----------------------
+
+# Make an instance of the XCO class and define the start path 
+import xco 
+xc = xco.XCO(start_path = 'd:/xc_real_projects/xc_all_europe')
+
+# Get information of what will be downloaded
+xc.get_summary(params_json = 'download_all_europe_train_a.json')
+# xc.get_summary(params_json = 'download_all_europe_train_b.json')
+# xc.get_summary(params_json = 'download_all_europe_train_c.json')
+# make summary tables 
+print(xc.df_recs.shape)
+# keep only if fs large enough 
+sel = xc.df_recs['smp'].astype(int)>= 24000
+xc.df_recs = xc.df_recs[sel]
+# Download the files 
+xc.download()
+# Convert mp3s to wav with a specific sampling rate (requires ffmpeg to be installed)
+xc.mp3_to_wav(conversion_fs = 24000)
+
+
+# # Make rectangular spectrogram with size = 128 freq x 256 time 
+# xc.extract_spectrograms(fs_tag = 24000, segm_duration = 0.394 , segm_step = 0.80, win_siz = 256, win_olap = 220.5, max_segm_per_file = 16, equalize = True, colormap='viridis')
+# Make long spectrogram 128 freq x 1152 time (1024+128= 1152)
+xc.extract_spectrograms(fs_tag = 24000, segm_duration = 1.738, segm_step = 0.95, win_siz = 256, win_olap = 220.00, max_segm_per_file = 20, equalize = True, colormap='viridis')
+
+
