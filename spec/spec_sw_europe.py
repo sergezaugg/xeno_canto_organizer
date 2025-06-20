7088245e08994139c9644005929f9d3ddad8fc38
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-20 15:24:37 +0200

    misc stuff in spec

--- spec/spec_sw_europe.py
+++ spec/spec_sw_europe.py
@@ -21,5 +21,7 @@ xc.download()
 xc.mp3_to_wav(conversion_fs = 24000)
 # Make rectangular spectrogram with size = 128 freq x 256 time 
 xc.extract_spectrograms(fs_tag = 24000, segm_duration = 0.394 , segm_step = 0.80, win_siz = 256, win_olap = 220.5, max_segm_per_file = 16, equalize = True, colormap='viridis')
+# Make square spectrogram with size = 128 freq x 128 time 
+xc.extract_spectrograms(fs_tag = 24000, segm_duration = 0.202 , segm_step = 0.80, win_siz = 256, win_olap = 220.5, max_segm_per_file = 16, equalize = True, colormap='viridis')
 
 
