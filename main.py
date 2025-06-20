cc6b18d90758b7c83e282476cd38b9ed2d9cc3c4
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-20 16:04:20 +0200

    implemented reload_local_summary

--- main.py
+++ main.py
@@ -13,37 +13,37 @@ if not os.path.isdir('./temp_xc_project'):
     os.makedirs('./temp_xc_project')
 # Make an instance of the XCO class and define the start path 
 xc = xco.XCO(start_path = './temp_xc_project')
+
 # Check where data will be retrieved
 xc.XC_API_URL
 # Check where data will be written 
 xc.start_path
+
 # Create a template json parameter file (to be edited)
 xc.make_param(filename = 'download_criteria.json', template = "mini")
 # Get information of what will be downloaded
 xc.download_summary(params_json = 'download_criteria.json')
 
-
-# reload summary to attribute
+# reload summary from local pkl file
 xc.reload_local_summary()
+
 # Make summaries  
 print(xc.df_recs.shape)
 print(xc.df_recs.head(10))
 
-
-
-# keep only if fs large enough 
+# list can be filtered based on metadata
+# e.g. keep only if fs large enough 
 sel = xc.df_recs['smp'].astype(int)>= 24000
 xc.df_recs = xc.df_recs[sel]
 
-
-
-
 # Download the files 
 xc.download_audio_files()
+
 # Convert mp3s to wav with a specific sampling rate (requires ffmpeg to be installed)
 xc.mp3_to_wav(conversion_fs = 24000)
 # Extract spectrograms from segments and store as PNG
 xc.extract_spectrograms(fs_tag = 24000, segm_duration = 1.0, segm_step = 0.5, win_siz = 512, win_olap = 192, max_segm_per_file = 12, equalize = True, colormap='viridis', eps = 1e-10)
+
 # Re-load and explore meta-data
 df_meta = pd.read_pickle(os.path.join(xc.start_path, 'downloaded_data_meta.pkl'))
 df_meta.head()
